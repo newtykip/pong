0e82d29656326f6210d495ec8c5dd0714d05fb0f
newt <hi@newty.dev>
2021-09-12 22:23:22 +0100

    Use original audio files!

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public GameObject audioSourceStore;
+    private AudioSource[] audioSources;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        audioSource = GetComponent<AudioSource>();
+        // Get one of the audio sources in the game object
+        audioSources = audioSourceStore.GetComponents<AudioSource>();
+        System.Random random = new System.Random();
+        int index = random.Next(audioSources.Length);
+        AudioSource audioSource = audioSources[index];
+
+        // Play it
         audioSource.Play();
     }
 }

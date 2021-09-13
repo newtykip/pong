542a07ac5b379a8acaafbf798a8edc76f4f76b52
newt! <newtykins@outlook.com>
2021-09-13 17:43:09 +0100

    Increase speed over time

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,87 +1,98 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
-    public GameObject ball;
-    public float speed = 5.0f;
-    public GameObject paddle1Object;
-    public GameObject paddle2Object;
-    private Paddle paddle1;
-    private Paddle paddle2;
-    public Text paddle1Score;
-    public Text paddle2Score;
-    public Vector2 originalPosition;
-    private Rigidbody2D rigidBody;
+	public GameObject ball;
+	public float speed = 5.0f;
+	public GameObject paddle1Object;
+	public GameObject paddle2Object;
+	private Paddle paddle1;
+	private Paddle paddle2;
+	public Text paddle1Score;
+	public Text paddle2Score;
+	public Vector2 originalPosition;
+	private Rigidbody2D rigidBody;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Get RigidBody2D component of Ball
-        rigidBody = ball.GetComponent<Rigidbody2D>();
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Get RigidBody2D component of Ball
+		rigidBody = ball.GetComponent<Rigidbody2D>();
 
-        // Get the paddles
-        paddle1 = paddle1Object.GetComponent<Paddle>();
-        paddle2 = paddle2Object.GetComponent<Paddle>();
+		// Get the paddles
+		paddle1 = paddle1Object.GetComponent<Paddle>();
+		paddle2 = paddle2Object.GetComponent<Paddle>();
 
-        // Save the original position
-        originalPosition = new Vector2(ball.transform.position.x, ball.transform.position.y);
+		// Save the original position
+		originalPosition = new Vector2(ball.transform.position.x, ball.transform.position.y);
 
-        // Apply initial force
-        ApplyForce();
-    }
+		// Apply initial force
+		ApplyForce();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+	// Update is called once per frame
+	void Update()
+	{
+	}
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        // Increment score
-        switch (collision.gameObject.tag) {
-            case "Paddle1Border":
-                paddle2.score++;
-                paddle2Score.text = paddle2.score.ToString();
-                OnScored();
-                break;
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		// Increment score
+		switch (collision.gameObject.tag)
+		{
+			case "Paddle1Border":
+				paddle2.score++;
+				paddle2Score.text = paddle2.score.ToString();
+				OnScored();
+				break;
 
-            case "Paddle2Border":
-                paddle1.score++;
-                paddle1Score.text = paddle1.score.ToString();
-                OnScored();
-                break;
+			case "Paddle2Border":
+				paddle1.score++;
+				paddle1Score.text = paddle1.score.ToString();
+				OnScored();
+				break;
 
-            case "PaddleSegment":
-                switch (collision.gameObject.name) {
-                    case "Top":
-                        rigidBody.AddForce(new Vector2(0.0f, 1.0f), ForceMode2D.Impulse);
-                        break;
-                    case "Bottom":
-                        rigidBody.AddForce(new Vector2(0.0f, -1.0f), ForceMode2D.Impulse);
-                        break;
-                }
-                break;
-        }
-    }
+			case "PaddleSegment":
+				switch (collision.gameObject.name)
+				{
+					case "Top":
+						rigidBody.AddForce(new Vector2(0.0f, 1.0f), ForceMode2D.Impulse);
+						break;
+					case "Bottom":
+						rigidBody.AddForce(new Vector2(0.0f, -1.0f), ForceMode2D.Impulse);
+						break;
+				}
 
-    void OnScored() {
-        // Reset the paddles
-        paddle1.transform.position = paddle1.paddle1OriginalPosition;
-        paddle2.transform.position = paddle2.paddle2OriginalPosition;
+				// Increment the speed regardless
+				speed += 0.25f;
+				rigidBody.velocity = new Vector2((rigidBody.velocity.x > 0 ? 1.0f : -1.0f) * speed, rigidBody.velocity.y);
 
-        // Reset the ball
-        ball.transform.position = originalPosition;
-        rigidBody.velocity = Vector2.zero;
-        ApplyForce();
-    }
+				break;
+		}
+	}
 
-    void ApplyForce() {
-        System.Random random = new System.Random();
-        List<float> directions = new List<float>{ -1.0f, 1.0f };
-        int index = random.Next(directions.Count);
+	void OnScored()
+	{
+		// Reset the paddles
+		paddle1.transform.position = paddle1.paddle1OriginalPosition;
+		paddle2.transform.position = paddle2.paddle2OriginalPosition;
 
-        rigidBody.AddForce(new Vector2(directions[index], UnityEngine.Random.Range(-1.0f, 1.0f)) * speed, ForceMode2D.Impulse);
-    }
+		// Reset the ball
+		ball.transform.position = originalPosition;
+		rigidBody.velocity = Vector2.zero;
+		speed = 5.0f;
+
+		ApplyForce();
+	}
+
+	void ApplyForce()
+	{
+		System.Random random = new System.Random();
+		List<float> directions = new List<float> { -1.0f, 1.0f };
+		int index = random.Next(directions.Count);
+
+		rigidBody.AddForce(new Vector2(directions[index], UnityEngine.Random.Range(-1.0f, 1.0f)) * speed, ForceMode2D.Impulse);
+	}
 }
--- Assets/Scripts/KeepInBounds.cs
+++ Assets/Scripts/KeepInBounds.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class KeepInBounds : MonoBehaviour
 {
-    public Camera MainCamera;
-    private Vector2 screenBounds;
-    private float objectWidth;
-    private float objectHeight;
+	public Camera MainCamera;
+	private Vector2 screenBounds;
+	private float objectWidth;
+	private float objectHeight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Border collision detection calculations
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; // extents = size of width / 2
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; // extents = size of height / 2
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Border collision detection calculations
+		screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+		objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; // extents = size of width / 2
+		objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; // extents = size of height / 2
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Border collision detection logic
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
-        transform.position = viewPos;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		// Border collision detection logic
+		Vector3 viewPos = transform.position;
+		viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+		viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+		transform.position = viewPos;
+	}
 }
